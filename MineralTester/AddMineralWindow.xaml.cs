1f49b831d6738b7ec20cce1d0f8a67f8f76b842c
QuinnNimmer <quinn.nimmer@gmail.com>
2021-11-08 21:26:51 -0600

    Changed labels and exiting

--- MineralTester/AddMineralWindow.xaml.cs
+++ MineralTester/AddMineralWindow.xaml.cs
@@ -41,6 +41,10 @@ namespace MineralTester.UI
             { 
                 EntryErrors(validFields);
             }
+            else
+            {
+                Close();
+            }
         }
 
         private void EntryErrors(List<bool> validFields)
