5df6f1ce4c265dc1d1141042991d7013bc00d6eb
QuinnNimmer <quinn.nimmer@gmail.com>
2021-12-13 17:57:21 -0600

    added class for password hashing/salting

---
+++ MineralTester.Classes/SecurityHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineralTester.Classes
+{
+    class SecurityHelper
+    {
+        public static string GenerateSalt()
+        {
+            var saltBytes = new byte[70];
+
+            using (var provider = new RNGCryptoServiceProvider())
+            {
+                provider.GetNonZeroBytes(saltBytes);
+            }
+
+            return Convert.ToBase64String(saltBytes);
+        }
+
+        public static string HashPassword(string password, string salt)
+        {
+            var saltBytes = Convert.FromBase64String(salt);
+
+            using (var rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, saltBytes, 10101))
+            {
+                return Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(70));
+            }
+        }
+
+    }
+}
