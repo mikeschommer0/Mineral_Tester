73c4fe48595c2b9dbad0b2f5c921b44332d0f594
QuinnNimmer <quinn.nimmer@gmail.com>
2021-12-13 22:59:13 -0600

    bug fixing

--- MineralTester/EditOrDeleteMineral.xaml.cs
+++ MineralTester/EditOrDeleteMineral.xaml.cs
@@ -63,6 +63,7 @@ namespace MineralTester.UI
                 {
                     FeedBackBox.Text = EntryErrors(validFields);
                 }
+
                 else
                 {
                     byte[] imgBytes = null;
